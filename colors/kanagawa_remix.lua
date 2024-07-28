3cc482eafe1eada51fe6a19eefb521621b457bf1
Cathyprime <magdajaroszynska47@gmail.com>
2024-07-28 18:29:42 +0200

    fix: naming

--- colors/kanagawa_remix.lua
+++ colors/kanagawa_remix.lua
@@ -1,21 +1,21 @@
 -- You probably always want to set this in your vim file
-vim.opt.background = 'dark'
-vim.g.colors_name = 'kanagawa_remix'
+-- vim.opt.background = 'dark'
+if vim.opt.background == "dark" then
+    vim.g.colors_name = 'kanagawa_remix_dark'
 
--- By setting our module to nil, we clear lua's cache,
--- which means the require ahead will *always* occur.
---
--- This isn't strictly required but it can be a useful trick if you are
--- incrementally editing your config a lot and want to be sure your themes
--- changes are being picked up without restarting neovim.
---
--- Note if you're working in on your theme and have :Lushify'd the buffer,
--- your changes will be applied with our without the following line.
---
--- The performance impact of this call can be measured in the hundreds of
--- *nanoseconds* and such could be considered "production safe".
-package.loaded['lush_theme.kanagawa_remix'] = nil
-
--- include our theme file and pass it to lush to apply
-require('lush')(require('lush_theme.kanagawa_remix'))
-
+    -- By setting our module to nil, we clear lua's cache,
+    -- which means the require ahead will *always* occur.
+    --
+    -- This isn't strictly required but it can be a useful trick if you are
+    -- incrementally editing your config a lot and want to be sure your themes
+    -- changes are being picked up without restarting neovim.
+    --
+    -- Note if you're working in on your theme and have :Lushify'd the buffer,
+    -- your changes will be applied with our without the following line.
+    --
+    -- The performance impact of this call can be measured in the hundreds of
+    -- *nanoseconds* and such could be considered "production safe".
+    package.loaded['lush_theme.kanagawa_remix_dark'] = nil
+    -- include our theme file and pass it to lush to apply
+    require('lush')(require('lush_theme.kanagawa_remix_dark'))
+end
